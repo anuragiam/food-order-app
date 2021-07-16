809a6e6b9dd7b67f2ffa73a8caf71df6ba44de77
Anurag <mailmeanurag2@gmail.com>
2021-07-17 04:39:31 +0530

    added cart items and add/remove to cart items

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,17 +1,43 @@
-import React from 'react'
-import CartIcon from '../Cart/CartIcon'
-import classes from './HeaderCardButton.module.css'
+import React, { useEffect, useState, useContext } from "react";
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
+import classes from "./HeaderCardButton.module.css";
 
 function HeaderCardButton(props) {
+  const [btnBump, setBtnBump] = useState(false);
+
+  const cartCtx = useContext(CartContext);
+  const {items}= cartCtx;
+
+  const numberOfCartItem = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${btnBump ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if(items.length === 0){
+      return;
+    }
+    setBtnBump(true);
+    const timer = setTimeout(()=>{
+      setBtnBump(false)
+    },300);
+
+    return ()=>{
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
-      <span className={classes.icon}><CartIcon/></span>
-      <span>Your Cart</span>
-      <span className={classes.badge}>
-        3
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
       </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItem}</span>
     </button>
-  )
+  );
 }
 
-export default HeaderCardButton
+export default HeaderCardButton;
